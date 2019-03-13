06592ca7789463c8de45a7d2634826bb1275bb56
Vadym Zaporozhets <v.zaporozhets@thomsonreuters.com>
2019-03-13 13:31:22 +0200

    install Redux, React-Redux, React-Router, add and configure jest

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {App} from './App';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+    const component = renderer.create(<App/>).toJSON();
+
+    expect(component).toMatchSnapshot();
+});
