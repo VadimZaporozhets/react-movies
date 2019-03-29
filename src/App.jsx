cb388802e480996c07014ac0ff6b2a57bb85c10b
Vadym Zaporozhets <v.zaporozhets@thomsonreuters.com>
2019-03-29 16:49:02 +0200

    Add Error boundary component, add plugin-proposal-class-properties plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HomeScene, DetailsScene } from './scenes';
 
-export const App = () => (
-    <Router>
-        <Switch>
-            <Route path="/" exact component={HomeScene} />
-            <Route path="/details" exact component={DetailsScene} />
-        </Switch>
-    </Router>
-);
+export class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { error: false, errorInfo: null };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({
+            errorInfo: errorInfo
+        });
+    }
+
+    render() {
+        if (this.state.errorInfo) {
+            return <h2>{this.state.errorInfo}</h2>;
+        } else {
+            return (
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={HomeScene} />
+                        <Route path="/details" exact component={DetailsScene} />
+                    </Switch>
+                </Router>
+            );
+        }
+    }
+}
