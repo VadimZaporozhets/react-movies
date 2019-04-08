b18c5a2a3fa057b62a02f2e8e0ce6aead8f77422
Vadym Zaporozhets <v.zaporozhets@thomsonreuters.com>
2019-04-08 09:29:10 -0400

    remove unwanted test, refactor overview param

--- src/Details/Details.test.js
+++ src/Details/Details.test.js
@@ -44,8 +44,7 @@ describe('<DetailsSceneComponent />', () => {
             vote_average: 0,
             releaseYear: '2019',
             genres: ['Action', 'Adventure', 'Fantasy', 'Science Fiction'],
-            overview:
-                "We all have a superhero inside us, it just takes a bit of magic to bring it out. In Billy Batson's case, by shouting out one word--SHAZAM!--this streetwise 14-year-old foster kid can turn into the adult superhero Shazam, courtesy of an ancient wizard.Still a kid at heart--inside a ripped, godlike body--Shazam revels in this adult version of himself by doing what any teen would do with superpowers: have fun with them! Can he fly? Does he have X-ray vision? Can he shoot lightning out of his hands? Can he skip his social studies test? Shazam sets out to test the limits of his abilities with the joyful recklessness of a child. But he'll need to master these powers quickly in order to fight the deadly forces of evil controlled by Doctor Thaddeus Sivana."
+            overview: 'mock'.repeat(100)
         };
 
         wrapper.setState({
--- src/Home/SearchPanel/SearchCategories/SearchCategories.test.js
+++ src/Home/SearchPanel/SearchCategories/SearchCategories.test.js
@@ -3,22 +3,14 @@ import { SearchCategoriesComponent } from './index';
 import { shallow } from 'enzyme';
 
 describe('<SearchCategoriesComponent />', () => {
-    let props, wrapper;
-
-    beforeEach(() => {
-        props = {
+    it('should render search categories component', () => {
+        const props = {
             classes: expect.any(Object),
             searchBy: 'Title'
         };
 
-        wrapper = shallow(<SearchCategoriesComponent {...props} />);
-    });
+        const wrapper = shallow(<SearchCategoriesComponent {...props} />);
 
-    it('should render search categories component', () => {
         expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should render sort by title button with contained variant', () => {
-        expect(wrapper.find('#search-by-title').prop('variant')).toEqual('contained');
     });
 });
